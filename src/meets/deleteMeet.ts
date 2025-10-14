82e464ef6cf1cd3ca4004fe02a029358b6542abd
anderson <anderdavid86@gmail.com>
2025-10-14 17:37:32 -0500

    se estructura crud meets

---
+++ src/meets/deleteMeet.ts
@@ -0,0 +1,22 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const handler = async (
+ event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+ try {
+  return {
+   statusCode: 201,
+   body: JSON.stringify('delete Meet'),
+  };
+ } catch (error) {
+  return {
+   statusCode: 500,
+   body: JSON.stringify({ message: 'Failed...' }),
+  };
+ }
+};
