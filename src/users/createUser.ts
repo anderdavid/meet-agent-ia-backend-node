299592d711652713b80e7a3b1ddc1ea253a8f068
anderson <anderdavid86@gmail.com>
2025-10-13 19:29:02 -0500

    se implementa validacion con zod

--- src/users/createUser.ts
+++ src/users/createUser.ts
@@ -1,10 +1,7 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import {
- DynamoDBDocumentClient,
- ScanCommand,
- PutCommand,
-} from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { z } from 'zod';
 import { v4 as uuidv4 } from 'uuid';
 
 const client = new DynamoDBClient({});
@@ -19,11 +16,19 @@ interface User {
  updateAt: number;
 }
 
+const userSchema = z.object({
+ name: z.string().min(6, 'El nombre es obligatorio'),
+ email: z.string().email('Debe ser un email válido'),
+ password: z.string().min(8, 'Password es obligatorio'),
+});
+
 export const handler = async (
  event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
  try {
   const body = JSON.parse(event.body || '{}');
+  const validated = userSchema.parse(body);
+
   const { name, email, password } = body;
 
   const user: User = {
@@ -47,6 +52,15 @@ export const handler = async (
   };
  } catch (error) {
   console.log('error: ', error);
+  if (error instanceof z.ZodError) {
+   return {
+    statusCode: 400,
+    body: JSON.stringify({
+     message: 'Error de validación',
+     errors: error.issues,
+    }),
+   };
+  }
   return {
    statusCode: 500,
    body: JSON.stringify({ message: 'Failed create user' }),
